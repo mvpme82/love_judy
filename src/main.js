7a90d7bcd9a01cbd003a85fb32b252b15bb05031
mengmeng <38303847+mvpme82@users.noreply.github.com>
2022-03-10 05:48:43 +0800

    今天第一次提交

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import {
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).use(ElementUI).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
